86ca09e7b34d6dcfd59f8ff6fc92b7209cce51bb
rafael <rafaeldsilva16@gmail.com>
2024-02-29 11:30:57 -0300

    feat: send project

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,7 +3,7 @@ import auth from "../core/adapters/routes/auth.js"
 import users from "../core/adapters/routes/users.js"
 
 import router from "../infrastructure/RouterInstance.js"
-export default (app, { db }) => {
+export default (app, { db }) => { 
     app.use("/", health(router, db))
     app.use("/auth", auth(router, db))
     app.use("/users", users(router, db))
